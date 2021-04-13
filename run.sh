d5f0ace34d794fc0ea02bd959b23006e9256c21c
Andrew J Warren <andrew.j.warren@oracle.com>
2021-04-12 22:50:22 -0500

    typo

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ BASE=$(pwd)
 
 cp $BASE/*.yaml $HA_CONFIG_DIR/
 
-find \( ! -regex '.*/\..*' \) -type d -maxdepth 1 -exec cp -R {} $HA_CONFIG_DIR/
+find \( ! -regex '.*/\..*' \) -type d -maxdepth 1 -exec cp -R {} $HA_CONFIG_DIR/ \;
